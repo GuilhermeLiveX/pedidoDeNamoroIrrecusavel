46cb14af18378c558341bf2433e0339de7ff7365
Guilherme Santana <guilhermehss17@gmail.com>
2023-08-26 21:06:13 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function sim() {
+  alert("Obrigado por namorar comigo, EU TE AMO ♥")
+  location.href = "https://music.youtube.com/watch?v=izGwDsrQ1eQ&si=IWODhX8KnlxfvOcW&t=2"
+}
+
+function desviar(btn) {
+  btn.style.position = "absolute"
+  btn.style.bottom = geraPosicao(5, 95)
+  btn.style.left = geraPosicao(5, 95)
+  console.log("Opa, desviei...")
+}
+
+function geraPosicao(min, max) {
+  return Math.random() * (max - min) + min + "%"
+}
